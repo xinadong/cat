a43014c96fe122f5196bc22d3f0135bdebd1f961
10Saturn16 <1427133058@qq.com>
2026-07-08 14:04:49 +0800

    Update Layout.vue
    更新了跟随鼠标移动的星星特效

--- animal-frontend/src/views/Layout.vue
+++ animal-frontend/src/views/Layout.vue
@@ -120,7 +120,7 @@
 </template>
 
 <script setup>
-import { computed } from 'vue'
+import { computed, onMounted, onUnmounted } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
 import { useUserStore } from '@/stores/user'
 
@@ -160,6 +160,168 @@ const handleCommand = (command) => {
     router.push('/')
   }
 }
+
+// ========== 鼠标跟随闪烁星星特效 ==========
+let sparkleTimer = null
+let mouseMoveHandler = null
+
+function initSparkles() {
+  const colour = 'random'
+  const sparkles = 50
+  let x = 400, ox = 400
+  let y = 300, oy = 300
+  let swide = window.innerWidth
+  let shigh = window.innerHeight
+  let sdown = 0
+  let sleft = 0
+  const tiny = []
+  const star = []
+  const starv = []
+  const starx = []
+  const stary = []
+  const tinyx = []
+  const tinyy = []
+  const tinyv = []
+
+  function createDiv(height, width) {
+    const div = document.createElement('div')
+    div.style.position = 'absolute'
+    div.style.height = height + 'px'
+    div.style.width = width + 'px'
+    div.style.overflow = 'hidden'
+    div.style.pointerEvents = 'none'
+    div.style.zIndex = '99999'
+    return div
+  }
+
+  function newColour() {
+    const c = new Array(3)
+    c[0] = 255
+    c[1] = Math.floor(Math.random() * 256)
+    c[2] = Math.floor(Math.random() * (256 - c[1] / 2))
+    c.sort(function () { return 0.5 - Math.random() })
+    return 'rgb(' + c[0] + ', ' + c[1] + ', ' + c[2] + ')'
+  }
+
+  function updateStar(i) {
+    if (--starv[i] === 25) star[i].style.clip = 'rect(1px, 4px, 4px, 1px)'
+    if (starv[i]) {
+      stary[i] += 1 + Math.random() * 3
+      if (stary[i] < shigh + sdown) {
+        star[i].style.top = stary[i] + 'px'
+        starx[i] += (i % 5 - 2) / 5
+        star[i].style.left = starx[i] + 'px'
+      } else {
+        star[i].style.visibility = 'hidden'
+        starv[i] = 0
+        return
+      }
+    } else {
+      tinyv[i] = 50
+      tiny[i].style.top = (tinyy[i] = stary[i]) + 'px'
+      tiny[i].style.left = (tinyx[i] = starx[i]) + 'px'
+      tiny[i].style.width = '2px'
+      tiny[i].style.height = '2px'
+      tiny[i].style.backgroundColor = star[i].childNodes[0].style.backgroundColor
+      star[i].style.visibility = 'hidden'
+      tiny[i].style.visibility = 'visible'
+    }
+  }
+
+  function updateTiny(i) {
+    if (--tinyv[i] === 25) {
+      tiny[i].style.width = '1px'
+      tiny[i].style.height = '1px'
+    }
+    if (tinyv[i]) {
+      tinyy[i] += 1 + Math.random() * 3
+      if (tinyy[i] < shigh + sdown) {
+        tiny[i].style.top = tinyy[i] + 'px'
+        tinyx[i] += (i % 5 - 2) / 5
+        tiny[i].style.left = tinyx[i] + 'px'
+      } else {
+        tiny[i].style.visibility = 'hidden'
+        tinyv[i] = 0
+        return
+      }
+    } else {
+      tiny[i].style.visibility = 'hidden'
+    }
+  }
+
+  function sparkle() {
+    if (x !== ox || y !== oy) {
+      ox = x
+      oy = y
+      for (let c = 0; c < sparkles; c++) {
+        if (!starv[c]) {
+          star[c].style.left = (starx[c] = x) + 'px'
+          star[c].style.top = (stary[c] = y) + 'px'
+          star[c].style.clip = 'rect(0px, 5px, 5px, 0px)'
+          star[c].childNodes[0].style.backgroundColor =
+            star[c].childNodes[1].style.backgroundColor =
+              colour === 'random' ? newColour() : colour
+          star[c].style.visibility = 'visible'
+          starv[c] = 50
+          break
+        }
+      }
+    }
+    for (let c = 0; c < sparkles; c++) {
+      if (starv[c]) updateStar(c)
+      if (tinyv[c]) updateTiny(c)
+    }
+    sparkleTimer = setTimeout(sparkle, 40)
+  }
+
+  for (let i = 0; i < sparkles; i++) {
+    const rats = createDiv(3, 3)
+    rats.style.visibility = 'hidden'
+    document.body.appendChild(tiny[i] = rats)
+    starv[i] = 0
+    tinyv[i] = 0
+    const rats2 = createDiv(5, 5)
+    rats2.style.backgroundColor = 'transparent'
+    rats2.style.visibility = 'hidden'
+    const rlef = createDiv(1, 5)
+    const rdow = createDiv(5, 1)
+    rats2.appendChild(rlef)
+    rats2.appendChild(rdow)
+    rlef.style.top = '2px'
+    rlef.style.left = '0px'
+    rdow.style.top = '0px'
+    rdow.style.left = '2px'
+    document.body.appendChild(star[i] = rats2)
+  }
+  sparkle()
+
+  mouseMoveHandler = function (e) {
+    y = e.pageY
+    x = e.pageX
+    sdown = window.pageYOffset
+    sleft = window.pageXOffset
+  }
+  document.addEventListener('mousemove', mouseMoveHandler)
+
+  window.addEventListener('resize', function () {
+    swide = window.innerWidth
+    shigh = window.innerHeight
+  })
+}
+
+function destroySparkles() {
+  if (sparkleTimer) clearTimeout(sparkleTimer)
+  if (mouseMoveHandler) document.removeEventListener('mousemove', mouseMoveHandler)
+  document.querySelectorAll('[style*="z-index: 99999"]').forEach(el => el.remove())
+}
+
+onMounted(() => {
+  initSparkles()
+})
+
+onUnmounted(() => {
+  destroySparkles()
+})
 </script>
 
 <style scoped>
